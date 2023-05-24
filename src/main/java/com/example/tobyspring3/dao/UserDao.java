a20fa5d44bcc83d0af16be320186ad324faf4ec6
gyuh2 <rbgmlrbfl@naver.com>
2023-05-24 15:42:07 +0900

    feat: main() 메소드 분리 - UserDaoTest 추가

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -46,20 +46,4 @@ public class UserDao {
 
         return user;
     }
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ConnectionMaker cm = new DConnectionMaker();
-        UserDao userDao = new UserDao(cm);
-        User user = new User();
-        user.setId("5");
-        user.setName("horse");
-        user.setPassword("222222");
-        userDao.add(user);
-
-        User selectedUser = userDao.get("5");
-        System.out.println(selectedUser.getId());
-        System.out.println(selectedUser.getName());
-        System.out.println(selectedUser.getPassword());
-
-    }
 }
